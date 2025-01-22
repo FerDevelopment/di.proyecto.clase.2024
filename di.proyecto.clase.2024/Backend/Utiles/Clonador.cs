d1a86b9ff7e686c8fa3a6b0a0b65a813fc206e4c
FerDevelopment <fernandoaaronmb@gmail.com>
2025-01-22 12:27:59 +0100

    Dividir los mvc con los mvvm y teminar de crear las vistas para editar y etc.

---
+++ di.proyecto.clase.2024/Backend/Utiles/Clonador.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace di.proyecto.clase._2024.Backend.Utiles
+{
+    
+
+    public static class Clonador
+    {
+        public static T ClonarDatos<T>(T origen) where T : new()
+        {
+            if (origen == null)
+                throw new ArgumentNullException(nameof(origen));
+
+            T destino = new T();
+            Type tipo = typeof(T);
+
+            foreach (PropertyInfo propiedad in tipo.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (propiedad.CanRead && propiedad.CanWrite)
+                {
+                    object valor = propiedad.GetValue(origen);
+                    propiedad.SetValue(destino, valor);
+                }
+            }
+
+            return destino;
+        }
+    }
+
+}
